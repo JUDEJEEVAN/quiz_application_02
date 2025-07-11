5eb547e748a4215a36b22fc4ed3ac687d43a0862
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-07-11 20:18:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_application_02/views/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: StartScreen()),
+  );
+}
