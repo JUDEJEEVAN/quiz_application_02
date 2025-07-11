30e408e3e4b080abb07b34fc5c1c7becc724192b
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-07-11 21:09:37 +0530

    start screen created and modified to look like the design.

--- lib/views/start_screen.dart
+++ lib/views/start_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_application_02/components/button.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xff01356B),
+              Color(0xff014285),
+              Color(0xff0260C2),
+              Color(0xff0268D1),
+            ],
+          ),
+        ),
+        // color: Colors.amber,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              // to add gaps above the other widgets
+              SizedBox(height: 50),
+
+              // simple text
+              Text(
+                ('quiz time').toUpperCase(),
+                style: TextStyle(color: Colors.white, fontSize: 36),
+              ),
+
+              // image
+              Image.asset('assets/home_image.png', width: 304),
+
+              // button
+              // Button(),
+              // InkWell(
+              //   child: Button(),
+              //   onTap: () {
+              //     print('clicked the button');
+              //   },
+              // ),
+              GestureDetector(
+                child: Button(),
+                onTap: () {
+                  print('gesture detector .....');
+                },
+              ),
+
+              // to add gaps below the other widgets
+              SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
