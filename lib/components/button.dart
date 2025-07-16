dd2e50d4179ba2ea0efbb7e94d814a4e63f5fa08
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-07-16 21:05:11 +0530

    button now accepts String labels and displays that label in the UI(within the button). It also accepts an optional function which will be triggered when the user taps on the button widget.

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  const Button({super.key});
+  final String label;
+  final Function()? onTap;
+
+  const Button({super.key, required this.label, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 51,
-      width: 303,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: Center(
-        child: Text(
-          ('START QUIZ').toUpperCase(),
-          style: TextStyle(color: Color(0xff01356B), fontSize: 16),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 51,
+        width: 303,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: TextStyle(color: Color(0xff01356B), fontSize: 16),
+          ),
         ),
       ),
     );
