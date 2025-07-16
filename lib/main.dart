a672825e729bbca71b5e3a7105e08607d1d3497e
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-07-16 21:07:10 +0530

    temporary change to questionScreen for dev.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_application_02/views/question_screen.dart';
 import 'package:quiz_application_02/views/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(debugShowCheckedModeBanner: false, home: StartScreen()),
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: QuestionScreen(),
+    ),
   );
 }
