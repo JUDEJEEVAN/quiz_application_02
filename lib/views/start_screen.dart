d5a1ecb7454a9c3cda44c4d30ca38f9dbb2303a6
Jude Jeevan Mahendran <judejeevan.g1@gmail.com>
2025-07-16 21:06:18 +0530

    the usage of button widget has been updated according to the newly updated button widget!

--- lib/views/start_screen.dart
+++ lib/views/start_screen.dart
@@ -48,12 +48,32 @@ class StartScreen extends StatelessWidget {
               //     print('clicked the button');
               //   },
               // ),
-              GestureDetector(
-                child: Button(),
+              // GestureDetector(
+              //   child: Button(label: 'Start Quiz New'),
+              //   onTap: () {
+              //     print('gesture detector .....');
+              //   },
+              // ),
+
+              // custom button
+              Button(
+                label: 'Start New Quiz',
                 onTap: () {
-                  print('gesture detector .....');
+                  print('running onAction function...');
                 },
               ),
+
+              // button plan
+              // 1. button kku label pass panni antha label display aahira maathiri seiya venum
+              // 2. button kku oru function pass panni atha buttona user tap pannekkulla run pannonum
+              //    2.1 button kku oru function pass panna koodiya maathiri irukkonum
+              //    2.2 antha function button tap pannekkulla run aahonum
+
+              // Button(
+              //  onTap: () {
+              //     print('gesture detector .....');
+              //  }
+              // ),
 
               // to add gaps below the other widgets
               SizedBox(height: 50),
@@ -64,3 +84,97 @@ class StartScreen extends StatelessWidget {
     );
   }
 }
+
+/**
+
+
+ int addTwoNumbers({required int numberA, required int numberB}) {
+    return numberA + numberB;
+ }
+
+ addTwoNumbers(numberB: 10, numberA: 12);
+
+
+ // factory la car manufacture panran 
+ wheels, stering, body, brake, engine
+
+  person 1 - wheels
+  person 2 - stering
+  persong 3 - body
+  person 4 - brake
+  person 5 - engine
+
+  car {
+    wheels = '2 set rubber';
+    stering = 'carbon fiber 1 wheel';
+    body = aluminium;
+    brake = 2 set hydrolic brake;
+    engine = 4 stroke 4 cylinder 700cc;
+
+    honk() {
+        // make sound
+    }
+
+    brake() {
+      // apply brake
+    }
+
+    accelerate() {
+      // increase speed
+    }
+
+  }
+
+
+  class Car {
+    String wheels;
+    String stering;
+    String body;
+    String brake;
+    String engine;
+    int numberOfWindows;
+    bool isFourWheelDrivable;
+
+    // constructor
+    Car({
+        required this.wheels, 
+        required this.stering, 
+        required this.body, 
+        requied this.brake, 
+        required this.engine, 
+        required this.numberOfWindws, 
+        required this.isFourWheelDrivable
+      });
+
+    void honk() {
+      print('Car is honking');
+    }
+
+    String brake() {
+      print('Stopping the car');
+      return 'Stopping the car';
+    }
+
+    double accelerate() {
+      print('increase speed');
+      return ++speed;
+    }
+  }
+
+  print();
+
+  Car(wheels: '2 set rubber', stering: 'carbon fiber 1 wheel', body: 'aluminium', ....)
+
+
+
+
+  functionName() {}
+  () {}
+
+  String getTitle() => ;
+  () => 
+
+  () => 
+  () {}
+
+ */
